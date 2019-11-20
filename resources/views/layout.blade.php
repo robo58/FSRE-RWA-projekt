c2e4bbac922b09d90a6889c5c05980c50e2bee44
robo58 <robertsliskovic@gmail.com>
2019-11-20 12:59:48 +0100

    change

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -45,6 +45,12 @@
           <li class="nav-item mx-0 mx-lg-1">
             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
           </li>
+          <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="{{ route('login') }}">Login</a>
+            </li>
+          <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="{{ route('register') }}">Register</a>
+            </li>
         </ul>
       </div>
     </div>
