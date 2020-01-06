3ed91252524671c74cd7843ad271fffd1d99ec19
robo58 <robertsliskovic@gmail.com>
2020-01-06 20:13:46 +0100

    d

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -22,7 +22,7 @@
                             @foreach($users as $user)
                             <tr>
                                 <th scope="row">{{$user->id}}</th>
-                                <td><a href="{{route('profile.kurac',$user->id)}}">{{$user->username}}</a></td>
+                                <td><a href="{{route('admin.users.index',$user->id)}}">{{$user->username}}</a></td>
                                 <td>{{$user->email}}</td>
                                 <td>{{ implode(', ' , $user->roles()->get()->pluck('name')->toArray()) }}</td>
                                 <td>
